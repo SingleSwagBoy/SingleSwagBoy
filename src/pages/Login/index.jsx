775e9a3d7d3cfc6a91e45c129727899b0aa4cadf
yinzhengchao <13882525297@163.com>
2021-07-06 16:40:42 +0800

    1

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -23,7 +23,7 @@ class Login extends Component {
   state={
     dingdParam: {
       "appid": "",
-      "redirect_uri": "mms/dashBoard",
+      "redirect_uri": "mms/login/ding",
       "uri": "",
     },
     devAppid:{
@@ -94,7 +94,9 @@ class Login extends Component {
     if (origin === "https://login.dingtalk.com") { //判断是否来自ddLogin扫码事件。
         let loginTmpCode = event.data; //拿到loginTmpCode后就可以在这里构造跳转链接进行跳转了
         let redirect_uri_check = `https://oapi.dingtalk.com/connect/oauth2/sns_authorize?appid=${info.appid}&response_type=code&scope=snsapi_login&state=STATE&redirect_uri=${info.redirect_uri}&loginTmpCode=${loginTmpCode}`
-        // window.location.href = (redirect_uri_check)
+        setTimeout((r)=>{
+          window.location.href = redirect_uri_check
+        },10000)
         console.log(redirect_uri_check,"redirect_uri_check")
     }
   }

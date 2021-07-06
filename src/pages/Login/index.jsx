71627b8b4fdfc6955b58920ab31196ae6bfb16fc
yinzhengchao <13882525297@163.com>
2021-07-06 14:40:50 +0800

    1

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -23,7 +23,7 @@ class Login extends Component {
   state={
     dingdParam: {
       "appid": "",
-      "redirect_uri": "new-mms/#/mms/artLists",
+      "redirect_uri": "new-mms/mms/artLists",
       "uri": "",
     },
     devAppid:{

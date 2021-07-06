f7cbd8265e9473d87ce09714a297838ce43a92c2
yinzhengchao <13882525297@163.com>
2021-07-06 15:19:50 +0800

    1

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -51,7 +51,7 @@ class Login extends Component {
     let base = "http://" + window.location.host;
     let a = this.state.dingdParam
     if (this.state.dingdParam.redirect_uri.indexOf(base) === -1){
-        a.redirect_uri = base + this.state.dingdParam.redirect_uri;
+        a.redirect_uri = base+"new-mms/#/" + this.state.dingdParam.redirect_uri;
         this.setState({
           dingdParam:a
         })

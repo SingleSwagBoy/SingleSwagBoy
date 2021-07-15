178ab8ba1cb327b31f03a4c894b83991cb01786e
yinzhengchao <13882525297@163.com>
2021-07-15 16:54:34 +0800

    1

--- src/index.js
+++ src/index.js
@@ -7,7 +7,13 @@ import { ConfigProvider } from 'antd'
 import zhCN from 'antd/lib/locale/zh_CN'
 import { Provider } from 'react-redux'
 import store from './store'
-
+String.prototype.replaceAll = function(targetStr, newStr) { //兼容360浏览器不兼容replaceAll的方法
+  var sourceStr = this.valueOf()
+  while (sourceStr.indexOf(targetStr) !== -1) {
+    sourceStr = sourceStr.replace(targetStr, newStr)
+  }
+  return sourceStr
+}
 
 render(
   <HashRouter>

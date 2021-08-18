8b3696e8d782ac2f2fbec3eb39fee3442ddd2c4c
yinzhengchao <13882525297@163.com>
2021-08-18 17:11:46 +0800

    合并地域新闻

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -14,137 +14,29 @@ const GrowConfig = lazy(()=>import('pages/level/growConfig/index.jsx'))
 const UserDemote = lazy(()=>import('pages/level/userDemote/index.jsx'))
 const BonusPayment = lazy(()=>import('pages/ayh/bonusPayment/index.jsx'))
 const AddressList = lazy(()=>import('pages/level/addressList/index.jsx'))
-const ServiceLog = lazy(()=>import('pages/service/serviceLog/index.jsx'))
-const ManageTag = lazy(()=>import('pages/service/manageTag/index.jsx'))
-const MiniInput = lazy(()=>import('pages/service/miniInput/index.jsx'))
-const RecommendPage = lazy (()=>import ('pages/recommend/index.jsx'));
-
-
+const AddressNews = lazy(()=>import('pages/configManage/addressNews/index.jsx'))
+// advertising management
 // const Test = lazy(() =>import('pages/test/test.jsx'));
 const adminRoutes = [
-    // {
-    //     path: '/mms/test',
-    //     component: Test,
-    //     name: '测试',
-    //     icon: LineChartOutlined
-    // },
-  {
-    path: '/mms/ayh/channel',
-    component: AyhChannel,
-    name: '奥运会节目单',
-    icon: LineChartOutlined
-  },
-  {
-    path: '/mms/ayh/winningNews',
-    component: WinningNews,
-    name: '夺奖快讯',
-    icon: LineChartOutlined
-  },
-  {
-    path: '/mms/ayh/sportsProgram',
-    component: SportsProgram,
-    name: '夺奖快讯',
-    icon: LineChartOutlined
-  },
-  {
-    path: '/mms/ayh/medalList',
-    component: MedalList,
-    name: '奖牌榜',
-    icon: LineChartOutlined
-  },
-  {
-    path: '/mms/ayh/eventList',
-    component: EventList,
-    name: '赛事列表',
-    icon: LineChartOutlined
-  },
-  {
-    path: '/mms/ayh/specialList',
-    component: SpecialList,
-    name: '赛事列表',
-    icon: LineChartOutlined
-  },
-  {
-    path: '/mms/ayh/shortVideo',
-    component: ShortVideo,
-    name: '短视频搜索',
-    icon: LineChartOutlined
-  },
-  {
-    path: '/mms/ayh/bonusPayment',
-    component: BonusPayment,
-    name: '奖金发放',
-    icon: LineChartOutlined
-  },
-  {
-    path: '/mms/level/equity',
-    component: Equity,
-    name: '权益配置',
-    icon: UnorderedListOutlined
-  },
-  {
-    path: '/mms/level/levelConfig',
-    component: LevelConfig,
-    name: '等级配置',
-    icon: UnorderedListOutlined
-  },
-  {
-    path: '/mms/level/growConfig',
-    component: GrowConfig,
-    name: '等级配置',
-    icon: UnorderedListOutlined
-  },
-  {
-    path: '/mms/level/userDemote',
-    component: UserDemote,
-    name: '用户降级',
-    icon: UnorderedListOutlined
-  },
-  {
-    path: '/mms/level/addressList',
-    component: AddressList,
-    name: '用户降级',
-    icon: UnorderedListOutlined
-  },
+  {path:'/mms/ayh/channel',component:AyhChannel,name:'奥运会节目单',icon:LineChartOutlined,code:"OlympicGames"},
+  {path:'/mms/ayh/winningNews',component:WinningNews,name:'夺奖快讯',icon:LineChartOutlined,code:"OlympicGames"},
+  {path:'/mms/ayh/sportsProgram',component:SportsProgram,name:'夺奖快讯',icon:LineChartOutlined,code:"OlympicGames"},
+  {path:'/mms/ayh/medalList',component:MedalList,name:'奖牌榜',icon:LineChartOutlined,code:"OlympicGames"},
+  {path:'/mms/ayh/eventList',component:EventList,name:'赛事列表',icon:LineChartOutlined,code:"OlympicGames"},
+  {path:'/mms/ayh/specialList',component:SpecialList,name:'赛事列表',icon:LineChartOutlined,code:"OlympicGames"},
+  {path:'/mms/ayh/shortVideo',component:ShortVideo,name:'短视频搜索',icon:LineChartOutlined,code:"OlympicGames"},
+  {path:'/mms/ayh/bonusPayment',component:BonusPayment,name:'奖金发放',icon:LineChartOutlined,code:"OlympicGames"},
 
-  {
-    path: '/mms/recommend/recommend',
-    component: RecommendPage,
-    name: '节目推荐',
-    icon: UnorderedListOutlined
-  },
-//   
+  {path:'/mms/level/equity',component:Equity,name:'权益配置',icon:UnorderedListOutlined,code:"LevelManage"},
+  {path:'/mms/level/levelConfig',component:LevelConfig,name:'等级配置',icon:UnorderedListOutlined,code:"LevelManage"},
+  {path:'/mms/level/growConfig',component:GrowConfig,name:'等级配置',icon:UnorderedListOutlined,code:"LevelManage"},
+  {path:'/mms/level/userDemote',component:UserDemote,name:'用户降级',icon:UnorderedListOutlined,code:"LevelManage"},
+  {path:'/mms/level/addressList',component:AddressList,name:'用户降级',icon:UnorderedListOutlined,code:"LevelManage"},
 
-  {
-    path: '/mms/service/serviceLog',
-    component: ServiceLog,
-    name: '服务分类',
-    icon: UnorderedListOutlined
-  },
-  {
-    path: '/mms/service/manageTag/:categoryId',
-    component: ManageTag,
-    name: '管理类别',
-    icon: UnorderedListOutlined
-  },
-  {
-    path: '/mms/service/miniInput',
-    component: MiniInput,
-    name: '小程序录入',
-    icon: UnorderedListOutlined
-  },
-  {
-    path: '/mms/noPermission',
-    component: NoPermission,
-    name: '没有权限',
-    meta: {
-      isNav: false,
-      roles: '*'
-    }
-  },
+  {path:'/mms/configManage/addressNews',component:AddressNews,name:'地域新闻',icon:UnorderedListOutlined,code:"configManage"},
+  {path:'/mms/noPermission',component:NoPermission,name:'没有权限',meta:{isNav:false,roles:'*'}}
 
-
-
+  
 ]
 
 export default adminRoutes

3e66b2a2af5ee946878a1a1fe9e4eb35c418f795
HuangQS <515215310@qq.com>
2021-08-19 15:38:11 +0800

    添加 配置添加逻辑

--- src/pages/config/doc.jsx
+++ src/pages/config/doc.jsx
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
-import { Breadcrumb, Badge, Input, DatePicker, Form, Button, Tooltip, Table, Pagination, Modal, Upload, Image, Select, Alert, notification, message, Divider } from 'antd';
+import { Breadcrumb, Badge, Input, Form, Button, Tooltip, Table, Pagination, Modal, Upload, Image, Select, Alert, notification, message, Divider } from 'antd';
 import {
+    baseUrl,
     requestConfigAddDoc,                //配置列表-添加配置
     requestConfigDocList,               //配置列表-配置列表
     requestConfigDeleteDoc,             //配置列表-删除配置
     requestConfigUpdateDoc,             //配置列表-更新配置
-    requestConfigDocKeyList,            //配置列表-配置文件Key列表
 } from 'api'
 import './doc_style.css'
-
 const { Option } = Select;
 
 export default class Doc extends Component {
     constructor(props) {
         super(props);
+
+        this.formRef = React.createRef();
         this.state = {
             table_box: {
                 table_layer: [],                //表格层级 根据层级更新表格title
@@ -36,10 +37,10 @@ export default class Doc extends Component {
                 { key: 2, value: '单项配置值' },
                 { key: 3, value: '多项配置值' },
             ],
+            //弹出框
             modal_box: {
-                title: '',
                 is_show: false,
-                formRef: null,      //数据映射
+                title: '新增',
             },
             temp: true,
         }
@@ -47,8 +48,10 @@ export default class Doc extends Component {
 
     render() {
         let { table_box, modal_box } = this.state;
+        let that = this;
         return (
             <div>
+                <div>{JSON.stringify(table_box.table_layer)}</div>
                 <Divider orientation="left">
                     <Breadcrumb>
                         <Breadcrumb.Item>列表</Breadcrumb.Item>
@@ -70,7 +73,7 @@ export default class Doc extends Component {
                         </Tooltip>
 
                         <Tooltip title='新增数据' placement="top"  >
-                            <Button onClick={() => this.onItemShowModalClick(null)} type="primary" style={{ 'marginLeft': '10px' }} >新增</Button>
+                            <Button onClick={() => this.onItemShowModalClick()} type="primary" style={{ 'marginLeft': '10px' }} >新增</Button>
                         </Tooltip>
                     </div>
                 }>
@@ -85,41 +88,43 @@ export default class Doc extends Component {
                             onChange={(page, pageSize) => this.onPageChange(page, pageSize)} />
                     </div>
                 }
-
-
-
-
-                <Modal title={modal_box.title} visible={modal_box.is_show} forceRender={true} onOk={() => this.onModalOkClick()} onCancel={() => this.onModalCancelClick()}>
+                {/* 弹出框 多层数据新增弹框 */}
+                <Modal title={modal_box.title} visible={modal_box.is_show} forceRender={true} width={800} onOk={() => this.onModalOkClick()} onCancel={() => this.onModalCancelClick()}>
                     {/* 第一层 */}
                     <Form labelCol={{ span: 6 }} wrapperCol={{ span: 16 }} ref={this.formRef}>
                         {
-                            table_box.table_layer.length == 0 &&
+                            table_box.table_layer.length === 0 &&
                             <div>
                                 <Form.Item label="名称" name='name' rules={[{ required: true, message: '请输入名称' }]}>
                                     <Input className="input-wrapper-from" placeholder="请输入名称" />
                                 </Form.Item>
-                                <Form.Item label="编码" name='time' rules={[{ required: true, message: '请输入编码' }]}  >
+                                <Form.Item label="编码" name='code' rules={[{ required: true, message: '请输入编码' }]}  >
                                     <Input className="input-wrapper-from" placeholder="请输入编码" />
                                 </Form.Item>
                             </div>
                         }
                         {
-                            table_box.table_layer.length == 1 &&
+                            table_box.table_layer.length === 1 &&
                             <div>
                                 <Form.Item label="key名称" name='name' rules={[{ required: true, message: '请输入名称' }]}>
                                     <Input className="input-wrapper-from" placeholder="请输入名称" />
                                 </Form.Item>
-                                <Form.Item label="key编码" name='time' rules={[{ required: true, message: '请输入编码' }]}  >
+                                <Form.Item label="key编码" name='code' rules={[{ required: true, message: '请输入编码' }]}  >
                                     <Input className="input-wrapper-from" placeholder="请输入编码" />
                                 </Form.Item>
                             </div>
                         }
                         {
-                            table_box.table_layer.length == 2 &&
+                            table_box.table_layer.length === 2 &&
                             <div>
-                                <Form.Item label="方案value" name='time' rules={[{ required: true, message: '请输入编码' }]}  >
-                                    <Upload>上传图片</Upload>
+                                <Form.Item label="参数" name='value' rules={[{ required: true, message: '请输入参数' }]}>
+                                    <Input className="input-wrapper-from" placeholder="请输入参数"></Input>
                                 </Form.Item>
+
+                                <Upload  {...this.buildAdImageUpload()}>
+                                    <Button>快捷上传</Button>
+                                </Upload>
+                                {that.formRef.current.getFieldValue("value") ? <Image height={100} src={that.formRef.current.getFieldValue("value")} /> : ''}
                             </div>
                         }
                     </Form>
@@ -165,37 +170,19 @@ export default class Doc extends Component {
         if (layer_count === 0 || layer_count === 1) {
             titles.push({
                 title: '文案名称', dataIndex: 'name', key: 'name', render: (rowValue, row, index) => {
-                    return (
-                        row.is_edit_name ?
-                            // <Input defaultValue={row.name} onBlur={(e) => { row.name = e.target.value ;}} /> :
-                            <Input defaultValue={row.name} onBlur={(e) => {
-                                let that = this;
-                                row.name = e.target.value;
-                                that.requestUpdateData(row)
-                            }} /> :
-
-                            <Button type='link' onClick={() => { row.is_edit_name = true; this.forceUpdate() }} >{row.name}</Button>
-                    )
+                    return (that.renderTitleItem(row, 'is_edit_name', 'name'))
                 }
             });
             titles.push({
                 title: '文案编码', dataIndex: 'code', key: 'code', render: (rowValue, row, index) => {
-                    return (
-                        row.is_edit ?
-                            <Input defaultValue={row.code} onChange={(e) => { row.code = e.target.value }} /> :
-                            <div>{row.code}</div>
-                    )
+                    return (that.renderTitleItem(row, 'is_edit_code', 'code'))
                 }
             });
         }
         else if (layer_count === 2) {
             titles.push({
                 title: '参数', dataIndex: 'value', key: 'value', render: (rowValue, row, index) => {
-                    return (
-                        row.is_edit ?
-                            <Input defaultValue={row.value} onChange={(e) => { row.value = e.target.value }} /> :
-                            <div >{row.value}</div>
-                    )
+                    return (that.renderTitleItem(row, 'is_edit_value', 'value'))
                 }
             });
         }
@@ -210,7 +197,7 @@ export default class Doc extends Component {
                     title: '类型', dataIndex: 'type', key: 'type',
                     render: (rowValue, row, index) => {
                         return (
-                            <Select defaultValue={row.type} style={{ width: 120 }} onChange={(select_id) => this.onItemTypeChange(select_id, row)}>
+                            <Select defaultValue={row.type} style={{ width: 220 }} onChange={(select_id) => this.onItemTypeChange(select_id, row)}>
                                 {dict_types.map((item, index) => {
                                     return <Option value={item.key} key={index}>{item.key} - {item.value}</Option>
                                 })}
@@ -224,7 +211,7 @@ export default class Doc extends Component {
                 title: '状态', dataIndex: 'status', key: 'status',
                 render: (rowValue, row, index) => {
                     return (
-                        <Select defaultValue={row.status} style={{ width: 120 }} onChange={(select_id) => this.onItemStatusChanged(select_id, row)}>
+                        <Select defaultValue={row.status} style={{ width: 220 }} onChange={(select_id) => this.onItemStatusChanged(select_id, row)}>
                             {dict_status.map((item, index) => {
                                 return <Option value={item.key} key={index}>{item.key} - {item.value}</Option>
                             })}
@@ -234,7 +221,7 @@ export default class Doc extends Component {
             });
         }
         titles.push({
-            title: '操作', dataIndex: 'action', key: 'action', width: 200, fixed: 'right',
+            title: '操作', dataIndex: 'action', key: 'action', width: 160, fixed: 'right',
             render: (rowValue, row, index) => {
                 let params_count = layer_count === 0 ? row.keyCount : layer_count === 1 ? row.valueCount : '';
                 return (
@@ -242,11 +229,11 @@ export default class Doc extends Component {
                         {/* <Button size='small' type="primary" type="link" onClick={() => { }}>复制</Button> */}
                         {layer_count < 2 &&
                             <Badge count={params_count} size="small">
-                                <Button size='small' type="primary" type="link" onClick={() => this.onItemManagerClick(row)}>管理</Button>
+                                <Button size='small' type="primary" type="link" onClick={() => this.onItemManagerClick(row)}>配置</Button>
                             </Badge>
                         }
-                        <Button size='small' type="primary" type="link" onClick={() => this.onItemEditClick(row)} style={{ marginLeft: 10 }}>{row.is_edit ? '保存' : '编辑'}</Button>
-                        <Button size='small' type="primary" type="link" onClick={() => this.onItemDeleteClick(row)}>删除</Button>
+                        {/* <Button size='small' type="primary" type="link" onClick={() => this.onItemEditClick(row)} style={{ marginLeft: 10 }}>{row.is_edit ? '保存' : '编辑'}</Button> */}
+                        <Button size='small' type="primary" type="link" style={{ marginLeft: 15 }} onClick={() => this.onItemDeleteClick(row)}>删除</Button>
                     </div>
                 )
             }
@@ -321,14 +308,14 @@ export default class Doc extends Component {
         let that = this;
         that.refreshList();
     }
-    //新增文案按钮被点击
-    onItemShowModalClick(item) {
+    //新增文案按钮被点击 展示输入框
+    onItemShowModalClick() {
         let that = this;
+        that.formRef.current.resetFields();
+
         let modal_box = that.state.modal_box;
         modal_box.is_show = true;
-        modal_box.title = item ? '编辑' : '新增';
         that.setState({ modal_box: modal_box })
-
     }
     //编辑按钮被点击
     onItemEditClick(item) {
@@ -342,6 +329,75 @@ export default class Doc extends Component {
         if (!item.is_edit) {
             that.requestUpdateData(item);
         }
+    }
+
+    //弹出框OK被点击
+    onModalOkClick() {
+        let that = this;
+        let object = that.formRef.current.getFieldsValue();
+
+        console.log(object);
+
+        let table_layer = that.state.table_box.table_layer;
+        let step = table_layer.length;
+
+        //配置列表 Key值列表
+        if (step === 0 || step === 1) {
+            let name = object.name;
+            if (!name) {
+                message.error('请填写名称');
+                return;
+            }
+            let code = object.code;
+            if (!code) {
+                message.error('请填写编码');
+                return;
+            }
+
+            //第二层
+            if (step === 1) {
+                let item = table_layer[step - 1];   //更新id
+                object.docId = item.id;
+                object.type = 2; // 默认单项
+            }
+        }
+        //value列表
+        else if (step === 2) {
+            object.value = that.formRef.current.getFieldValue('value'); //控件原因 暂时这么写
+            if (!object.value) {
+                message.error('请填写value值');
+                return;
+            }
+            let item = table_layer[step - 1];   //更新id
+            object.docKeyId = item.id;
+        }
+
+
+        requestConfigAddDoc(step, object).then(res => {
+            let errCode = res.data.errCode;
+            if (errCode === 0) {
+                message.success('数据添加成功');
+                //隐藏弹出框
+                let modal_box = that.state.modal_box;
+                modal_box.is_show = false;
+                that.setState({ modal_box: modal_box })
+                that.refreshList();
+            } else {
+                message.error('数据添加失败：' + res.data.meg)
+            }
+        })
+
+
+
+
+    }
+    //弹出框Cancel被点击
+    onModalCancelClick() {
+        let that = this;
+        let modal_box = that.state.modal_box;
+        modal_box.is_show = false;
+
+        that.setState({ modal_box: modal_box })
     }
 
 
@@ -371,19 +427,6 @@ export default class Doc extends Component {
         return '';
     }
 
-    //弹出框OK被点击
-    onModalOkClick() {
-
-    }
-    //弹出框Cancel被点击
-    onModalCancelClick() {
-        let that = this;
-        let modal_box = that.state.modal_box;
-        modal_box.is_show = false;
-
-        that.setState({ modal_box: modal_box })
-    }
-
     //类型变更
     onItemTypeChange(select_id, row) {
         let that = this;
@@ -398,6 +441,54 @@ export default class Doc extends Component {
         that.requestUpdateData(row);
     }
 
+    /**
+     * 输入框变更
+     * 
+     * @param {*} target_key 
+     * @param {*} e 
+     * @param {*} row 
+     */
+    onItemInputChanged(target_key, e, row) {
+        let that = this;
+        row[target_key] = e.target.value;
+
+
+        that.requestUpdateData(row)
+    }
+
+    /**
+     * 渲染表格item 默认为按钮，被点击后出现输入框、失去焦点后判断更新数据、刷新列表
+     * @param {*} row 
+     * @param {*} flag_key 
+     * @param {*} param_key 
+     * @returns 
+     */
+    renderTitleItem(row, flag_key, param_key) {
+        let that = this;
+        let is_edit = row[flag_key];
+        let default_value = row[param_key];
+
+        //编辑形态 失去焦点 更新数据
+        if (is_edit) {
+            return <Input defaultValue={default_value} onBlur={(e) => {
+                let new_value = e.target.value;
+                if (default_value === new_value) {
+                    row[flag_key] = false;
+                    that.forceUpdate();
+                    return;
+                }
+
+                row[param_key] = new_value;
+                that.requestUpdateData(row)
+            }} />
+        }
+        //按钮形态
+        else {
+            return <Button type='link' onClick={() => { row[flag_key] = true; that.forceUpdate(); }} >
+                {default_value}
+            </Button>
+        }
+    }
     //更新
     requestUpdateData(row) {
         let that = this;
@@ -416,4 +507,69 @@ export default class Doc extends Component {
         })
     }
 
+
+    //图片上传地址
+    buildAdImageUpload() {
+        let that = this;
+        return {
+            name: "file",
+            listType: "text",
+            className: "avatar-uploader",
+            showUploadList: false,
+            action: `${baseUrl}/mms/file/upload?dir=ad`,
+            headers: {
+                authorization: JSON.parse(localStorage.getItem("user")).authorization,
+            },
+            //上传文件改变时的状态
+            onChange(info) {
+                if (info.file.status !== 'uploading') {
+                    that.setState({ is_loading: true });
+                }
+                if (info.file.status === 'done') {
+                    let response = info.file.response;
+                    let errCode = response.errCode;
+                    if (errCode === 0) {
+                        let fileObj = info.file.originFileObj;
+                        that.getBase64(fileObj, imageUrl => {
+
+                            if (!info.file.response.data) {
+                                message.error(`${info.file.name} 上传失败.保持网络连接，图片不能超过大小限制。`);
+                                that.setState({ is_loading: false, });
+                                return;
+                            }
+                            message.success(`上传成功`);
+                            console.log('上传成功', info.file.response.data.fileUrl)
+                            that.formRef.current.setFieldsValue({ 'value': info.file.response.data.fileUrl })
+                            that.forceUpdate();
+                        })
+                    }
+                    else {
+                        let msg = response.msg;
+                        message.error(`上传失败` + msg);
+                    }
+                }
+                else if (info.file.status === 'error') {
+                    message.error(`${info.file.name} 上传失败.`);
+                }
+            },
+            //上传文件之前的钩子，参数为上传的文件
+            beforeUpload(file) {
+                let isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
+                if (!isJpgOrPng) {
+                    message.error('You can only upload JPG/PNG file!');
+                }
+                let isLt2M = file.size / 1024 / 1024 < 2;
+                if (!isLt2M) {
+                    message.error('Image must smaller than 2MB!');
+                }
+                return isJpgOrPng && isLt2M;
+            }
+        }
+    }
+
+    getBase64(img, callback) {
+        let reader = new FileReader();
+        reader.addEventListener('load', () => callback(reader.result));
+        reader.readAsDataURL(img);
+    }
 }

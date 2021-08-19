0a45bc947e53cc9c3c4a09ab30b8a00f1b3dc224
HuangQS <515215310@qq.com>
2021-08-19 12:59:57 +0800

    修改 表格数据快速修改更新逻辑

--- src/pages/config/doc.jsx
+++ src/pages/config/doc.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Breadcrumb, Badge, Input, DatePicker, Button, Tooltip, Table, Pagination, Modal, Image, Select, Alert, notification, message, Divider } from 'antd';
+import { Breadcrumb, Badge, Input, DatePicker, Form, Button, Tooltip, Table, Pagination, Modal, Upload, Image, Select, Alert, notification, message, Divider } from 'antd';
 import {
     requestConfigAddDoc,                //配置列表-添加配置
     requestConfigDocList,               //配置列表-配置列表
@@ -40,8 +40,8 @@ export default class Doc extends Component {
                 title: '',
                 is_show: false,
                 formRef: null,      //数据映射
-            }
-
+            },
+            temp: true,
         }
     }
 
@@ -51,7 +51,7 @@ export default class Doc extends Component {
             <div>
                 <Divider orientation="left">
                     <Breadcrumb>
-                        <Breadcrumb.Item>所有</Breadcrumb.Item>
+                        <Breadcrumb.Item>列表</Breadcrumb.Item>
                         {table_box.table_layer.map((item, index) => {
                             return <Breadcrumb.Item key={index}>{item.name}</Breadcrumb.Item>
                         })}
@@ -89,15 +89,40 @@ export default class Doc extends Component {
 
 
 
-                <Modal title={modal_box.title} visible={modal_box.is_show} onOk={() => this.onModalOkClick()} onCancel={() => this.onModalCancelClick()}>
+                <Modal title={modal_box.title} visible={modal_box.is_show} forceRender={true} onOk={() => this.onModalOkClick()} onCancel={() => this.onModalCancelClick()}>
                     {/* 第一层 */}
-                    <p>Some contents...</p>
-                    <p>Some contents...</p>
-                    <p>Some contents...</p>
-
-                    if(modal_box.title){
-                        <div>ads</div>
-                    }
+                    <Form labelCol={{ span: 6 }} wrapperCol={{ span: 16 }} ref={this.formRef}>
+                        {
+                            table_box.table_layer.length == 0 &&
+                            <div>
+                                <Form.Item label="名称" name='name' rules={[{ required: true, message: '请输入名称' }]}>
+                                    <Input className="input-wrapper-from" placeholder="请输入名称" />
+                                </Form.Item>
+                                <Form.Item label="编码" name='time' rules={[{ required: true, message: '请输入编码' }]}  >
+                                    <Input className="input-wrapper-from" placeholder="请输入编码" />
+                                </Form.Item>
+                            </div>
+                        }
+                        {
+                            table_box.table_layer.length == 1 &&
+                            <div>
+                                <Form.Item label="key名称" name='name' rules={[{ required: true, message: '请输入名称' }]}>
+                                    <Input className="input-wrapper-from" placeholder="请输入名称" />
+                                </Form.Item>
+                                <Form.Item label="key编码" name='time' rules={[{ required: true, message: '请输入编码' }]}  >
+                                    <Input className="input-wrapper-from" placeholder="请输入编码" />
+                                </Form.Item>
+                            </div>
+                        }
+                        {
+                            table_box.table_layer.length == 2 &&
+                            <div>
+                                <Form.Item label="方案value" name='time' rules={[{ required: true, message: '请输入编码' }]}  >
+                                    <Upload>上传图片</Upload>
+                                </Form.Item>
+                            </div>
+                        }
+                    </Form>
                 </Modal>
             </div>
         )
@@ -137,16 +162,42 @@ export default class Doc extends Component {
         titles.push({ title: 'id', dataIndex: 'id', key: 'id', width: 100, });
 
         let layer_count = table_layer.length;
-        if (layer_count === 0) {
-            titles.push({ title: '文案名称', dataIndex: 'name', key: 'name' });
-            titles.push({ title: '文案编码', dataIndex: 'code', key: 'code' });
-        }
-        else if (layer_count === 1) {
-            titles.push({ title: '文案key名称', dataIndex: 'name', key: 'name' });
-            titles.push({ title: '文案key编码', dataIndex: 'code', key: 'code' });
+        if (layer_count === 0 || layer_count === 1) {
+            titles.push({
+                title: '文案名称', dataIndex: 'name', key: 'name', render: (rowValue, row, index) => {
+                    return (
+                        row.is_edit_name ?
+                            // <Input defaultValue={row.name} onBlur={(e) => { row.name = e.target.value ;}} /> :
+                            <Input defaultValue={row.name} onBlur={(e) => {
+                                let that = this;
+                                row.name = e.target.value;
+                                that.requestUpdateData(row)
+                            }} /> :
+
+                            <Button type='link' onClick={() => { row.is_edit_name = true; this.forceUpdate() }} >{row.name}</Button>
+                    )
+                }
+            });
+            titles.push({
+                title: '文案编码', dataIndex: 'code', key: 'code', render: (rowValue, row, index) => {
+                    return (
+                        row.is_edit ?
+                            <Input defaultValue={row.code} onChange={(e) => { row.code = e.target.value }} /> :
+                            <div>{row.code}</div>
+                    )
+                }
+            });
         }
         else if (layer_count === 2) {
-            titles.push({ title: '文案value名称', dataIndex: 'value', key: 'value' });
+            titles.push({
+                title: '参数', dataIndex: 'value', key: 'value', render: (rowValue, row, index) => {
+                    return (
+                        row.is_edit ?
+                            <Input defaultValue={row.value} onChange={(e) => { row.value = e.target.value }} /> :
+                            <div >{row.value}</div>
+                    )
+                }
+            });
         }
 
         //第0层||第1层
@@ -159,9 +210,9 @@ export default class Doc extends Component {
                     title: '类型', dataIndex: 'type', key: 'type',
                     render: (rowValue, row, index) => {
                         return (
-                            <Select defaultValue={row.status} style={{ width: 120 }}>
+                            <Select defaultValue={row.type} style={{ width: 120 }} onChange={(select_id) => this.onItemTypeChange(select_id, row)}>
                                 {dict_types.map((item, index) => {
-                                    return <Option value={item.key} key={index}>{item.value}</Option>
+                                    return <Option value={item.key} key={index}>{item.key} - {item.value}</Option>
                                 })}
                             </Select>
                         )
@@ -173,9 +224,9 @@ export default class Doc extends Component {
                 title: '状态', dataIndex: 'status', key: 'status',
                 render: (rowValue, row, index) => {
                     return (
-                        <Select defaultValue={row.status} style={{ width: 120 }}>
+                        <Select defaultValue={row.status} style={{ width: 120 }} onChange={(select_id) => this.onItemStatusChanged(select_id, row)}>
                             {dict_status.map((item, index) => {
-                                return <Option value={item.key} key={index}>{item.value}</Option>
+                                return <Option value={item.key} key={index}>{item.key} - {item.value}</Option>
                             })}
                         </Select>
                     )
@@ -194,7 +245,7 @@ export default class Doc extends Component {
                                 <Button size='small' type="primary" type="link" onClick={() => this.onItemManagerClick(row)}>管理</Button>
                             </Badge>
                         }
-                        <Button size='small' type="primary" type="link" onClick={() => this.onItemShowModalClick(row)} style={{ marginLeft: 10 }}>编辑</Button>
+                        <Button size='small' type="primary" type="link" onClick={() => this.onItemEditClick(row)} style={{ marginLeft: 10 }}>{row.is_edit ? '保存' : '编辑'}</Button>
                         <Button size='small' type="primary" type="link" onClick={() => this.onItemDeleteClick(row)}>删除</Button>
                     </div>
                 )
@@ -224,9 +275,9 @@ export default class Doc extends Component {
         request_params.page = { currentPage: curr_page, pageSize: 1000, isPage: 9 };
 
         table_box.table_title = titles;
-        table_box.table_datas = [];
         that.setState({ table_box: table_box, });
 
+        table_box.table_datas = [];
 
         requestConfigDocList(layer_count, request_params).then(res => {
             let errCode = res.data.errCode;
@@ -277,7 +328,22 @@ export default class Doc extends Component {
         modal_box.is_show = true;
         modal_box.title = item ? '编辑' : '新增';
         that.setState({ modal_box: modal_box })
+
     }
+    //编辑按钮被点击
+    onItemEditClick(item) {
+        let that = this;
+        let is_edit = item.is_edit;
+        if (!is_edit) is_edit = false;
+        item.is_edit = !is_edit;
+        that.forceUpdate();
+
+        //保存
+        if (!item.is_edit) {
+            that.requestUpdateData(item);
+        }
+    }
+
 
     //返回上一层层级
     onLayerBackClick() {
@@ -316,6 +382,38 @@ export default class Doc extends Component {
         modal_box.is_show = false;
 
         that.setState({ modal_box: modal_box })
+    }
+
+    //类型变更
+    onItemTypeChange(select_id, row) {
+        let that = this;
+        row.type = select_id;
+        that.requestUpdateData(row);
+    }
+
+    //状态变更
+    onItemStatusChanged(select_id, row) {
+        let that = this;
+        row.status = select_id;
+        that.requestUpdateData(row);
+    }
+
+    //更新
+    requestUpdateData(row) {
+        let that = this;
+        let table_box = that.state.table_box;
+        let table_layer = table_box.table_layer;    //表格层级
+        let layer_count = table_layer.length;
+
+        requestConfigUpdateDoc(layer_count, row).then(res => {
+            let errCode = res.data.errCode;
+            if (errCode === 0) {
+                message.success('修改成功');
+                that.refreshList();
+            } else {
+                message.error('修改失败:' + res.data.message);
+            }
+        })
     }
 
 }

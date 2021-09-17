ebc842e7794a9066d02fc83c3a1eca1fb3d1a238
HuangQS <515215310@qq.com>
2021-09-17 18:03:49 +0800

    添加 微信支付模板页 弹出框

--- src/pages/wechart/templateMsg/wxTemplateMsg.jsx
+++ src/pages/wechart/templateMsg/wxTemplateMsg.jsx
@@ -2,13 +2,13 @@
  * @Author: HuangQS
  * @Date: 2021-08-30 11:56:33
  * @LastEditors: HuangQS
- * @LastEditTime: 2021-09-17 15:41:34
+ * @LastEditTime: 2021-09-17 18:00:37
  * @Description: 微信支付模板消息
  */
 
 
 import React, { Component } from 'react';
-import { Menu, Button, Table, Switch, Input, Upload, Image, message, Select, Alert, Tooltip, Form, Radio } from 'antd';
+import { Menu, Button, Table, Switch, Input, Upload, Image, message, Select, Alert, Modal, Form, DatePicker } from 'antd';
 import { MySyncBtn } from '@/components/views.js';
 
 import {
@@ -20,13 +20,16 @@ import {
     requestWxTemplateMsgConfigDelete,                           //微信模板消息 删除
     requestWxTemplateMsgConfigSend,                             //微信模板消息 测试发送
 } from 'api';
+import "@/style/base.css";
 
 let { Option } = Select;
-
+let { RangePicker } = DatePicker;
 
 export default class WxPayTemplate extends Component {
     constructor(props) {
         super(props);
+        this.formRef = React.createRef();
+
         this.state = {
             dict_public_types: [],  //字典 微信公众号回复类型
 
@@ -53,6 +56,48 @@ export default class WxPayTemplate extends Component {
                 } />
 
                 <Table columns={table_box.table_title} dataSource={table_box.table_datas} pagination={false} scroll={{ x: 1000, y: '75vh' }} />
+                <Modal visible={modal_box.is_show} title={modal_box.title} width={800} transitionName="" onCancel={() => that.onModalCancelClick()}
+                    footer={[
+                        <Button onClick={() => that.onModalCancelClick()}>取消</Button>,
+                        <Button onClick={() => that.onModalConfirmClick()} >确定</Button>
+                    ]}
+                >
+                    <Form labelCol={{ span: 6 }} wrapperCol={{ span: 16 }} ref={that.formRef}>
+                        {
+                            that.formRef && that.formRef.current &&
+                            <div>
+                                {
+                                    that.formRef.current.getFieldValue('Id') &&
+                                    <Form.Item label="id" name='Id' rules={[{ required: true }]} >
+                                        <Input className="base-input-wrapper" disabled />
+                                    </Form.Item>
+                                }
+                            </div>
+                        }
+
+                        <Form.Item label="上下线时间"  >
+                            <RangePicker showTime className="base-input-wrapper" />
+                        </Form.Item>
+
+                        <Form.Item label="播放时间段"  >
+
+                        </Form.Item>
+
+                        <Form.Item label="微信公众号" name='wxCode' >
+                            <Select className="base-input-wrapper">
+                                {dict_public_types.map((item, index) => (
+                                    <Option value={item.code} key={index}>{item.name}</Option>
+                                ))}
+                            </Select>
+
+                        </Form.Item>
+
+
+                    </Form>
+
+
+
+                </Modal>
             </div>
 
         )
@@ -82,7 +127,7 @@ export default class WxPayTemplate extends Component {
         let { table_box, dict_public_types } = that.state;
 
         let table_title = [
-            { title: 'Id', dataIndex: 'Id', key: '_id', width: 80, },
+            { title: 'id', dataIndex: 'Id', key: '_id', width: 80, },
             { title: '名称', dataIndex: 'Name', key: 'Name', width: 80, },
             // { title: 'Title', dataIndex: 'Title', key: 'Title', width: 80, },
             {
@@ -118,7 +163,6 @@ export default class WxPayTemplate extends Component {
                     return (<Switch defaultChecked={row.Status === 1 ? true : false} checkedChildren="是" unCheckedChildren="否" />)
                 }
             },
-
             {
                 title: '操作', dataIndex: 'action', key: 'action', fixed: 'right', width: 200,
                 render: (rowValue, row, index) => {
@@ -126,7 +170,7 @@ export default class WxPayTemplate extends Component {
                         <div>
                             <Button size='small' style={{ marginLeft: 3 }}>复制</Button>
                             <Button size='small' style={{ marginLeft: 3 }}>发送</Button>
-                            <Button size='small' style={{ marginLeft: 3 }}>编辑</Button>
+                            <Button size='small' style={{ marginLeft: 3 }} onClick={() => that.onItemEditClick(row)}>编辑</Button>
                             <Button size='small' style={{ marginLeft: 3 }}>删除</Button>
                         </div>
                     )
@@ -166,6 +210,48 @@ export default class WxPayTemplate extends Component {
     }
 
     onCreateClick() {
-
+        let that = this;
+        that.setState({
+            modal_box: {
+                is_show: true,
+                title: '新增',
+            }
+        }, () => {
+            that.forceUpdate();
+            that.formRef.current.resetFields();
+        })
     }
+
+    //编辑按钮被点击
+    onItemEditClick(item) {
+        let that = this;
+        that.setState({
+            modal_box: {
+                is_show: true,
+                title: '编辑',
+            }
+        }, () => {
+            that.forceUpdate();
+            that.formRef.current.resetFields();
+            that.formRef.current.setFieldsValue(item);
+        })
+    }
+
+
+    //弹出框取消按钮被点击
+    onModalCancelClick() {
+        let that = this;
+        that.setState({
+            modal_box: {
+                is_show: false,
+                title: '',
+            }
+        });
+    }
+
+    //弹出框确定按钮被点击
+    onModalConfirmClick() {
+    }
+
+
 }

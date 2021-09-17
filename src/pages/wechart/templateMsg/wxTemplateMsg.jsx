45a3050298781dee8a99c5d88b74c9571775ea8a
HuangQS <515215310@qq.com>
2021-09-17 16:41:39 +0800

    修改 微信支付模板消息 展示公众号类型

--- src/pages/wechart/templateMsg/wxTemplateMsg.jsx
+++ src/pages/wechart/templateMsg/wxTemplateMsg.jsx
@@ -2,7 +2,7 @@
  * @Author: HuangQS
  * @Date: 2021-08-30 11:56:33
  * @LastEditors: HuangQS
- * @LastEditTime: 2021-09-17 14:18:30
+ * @LastEditTime: 2021-09-17 15:41:34
  * @Description: 微信支付模板消息
  */
 
@@ -12,6 +12,8 @@ import { Menu, Button, Table, Switch, Input, Upload, Image, message, Select, Ale
 import { MySyncBtn } from '@/components/views.js';
 
 import {
+    requestWxReplyTypes,                                        //获取回复公众号的类型
+
     requestWxTemplateMsgConfigList,                             //微信模板消息 列表
     requestWxTemplateMsgConfigCreate,                           //微信模板消息 添加
     requestWxTemplateMsgConfigUpload,                           //微信模板消息 修改
@@ -19,11 +21,15 @@ import {
     requestWxTemplateMsgConfigSend,                             //微信模板消息 测试发送
 } from 'api';
 
+let { Option } = Select;
+
 
 export default class WxPayTemplate extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            dict_public_types: [],  //字典 微信公众号回复类型
+
             table_box: {
                 table_title: [],
                 table_datas: [],
@@ -36,10 +42,7 @@ export default class WxPayTemplate extends Component {
     }
     render() {
         let that = this;
-        let { table_box, modal_box } = that.state;
-        let input_width_size = 340;
-
-
+        let { table_box, modal_box, dict_public_types } = that.state;
         return (
             <div>
                 <Alert className="alert-box" message="微信模板消息" type="success" action={
@@ -48,7 +51,6 @@ export default class WxPayTemplate extends Component {
                         <MySyncBtn type={6} name={'同步缓存'} />
                     </div>
                 } />
-
 
                 <Table columns={table_box.table_title} dataSource={table_box.table_datas} pagination={false} scroll={{ x: 1000, y: '75vh' }} />
             </div>
@@ -62,18 +64,41 @@ export default class WxPayTemplate extends Component {
 
     initData() {
         let that = this;
-        that.initTitle();
+
+        requestWxReplyTypes().then(res => {
+            let types = res.data;
+
+            that.setState({
+                dict_public_types: types
+            }, () => {
+                that.initTitle();
+            });
+        })
+
     }
 
     initTitle() {
         let that = this;
-        let { table_box } = that.state;
+        let { table_box, dict_public_types } = that.state;
 
         let table_title = [
             { title: 'Id', dataIndex: 'Id', key: '_id', width: 80, },
             { title: '名称', dataIndex: 'Name', key: 'Name', width: 80, },
             // { title: 'Title', dataIndex: 'Title', key: 'Title', width: 80, },
-            { title: '微信公众号', dataIndex: 'WxCode', key: 'WxCode', width: 80, },
+            {
+                title: '微信公众号', dataIndex: 'WxCode', key: 'WxCode', width: 120,
+                render: (rowValue, row, index) => {
+                    return (
+                        <div>
+                            <Select disabled defaultValue={row.WxCode} style={{ width: '100%' }}>
+                                {dict_public_types.map((item, index) => (
+                                    <Option value={item.code} key={index}>{item.name}</Option>
+                                ))}
+                            </Select>
+                        </div>
+                    )
+                }
+            },
             // { title: '模板Id', dataIndex: 'TmplId', key: 'TmplId', width: 80, },
             // { title: '跳转地址', dataIndex: 'Jump', key: 'Jump', width: 80, },
             {
@@ -87,7 +112,13 @@ export default class WxPayTemplate extends Component {
                 }
             },
             { title: '投放时间段', dataIndex: 'TimeBucket', key: 'TimeBucket', width: 200, },
-         
+            {
+                title: '状态', dataIndex: 'Status', key: 'Status', width: 80,
+                render: (rowValue, row, index) => {
+                    return (<Switch defaultChecked={row.Status === 1 ? true : false} checkedChildren="是" unCheckedChildren="否" />)
+                }
+            },
+
             {
                 title: '操作', dataIndex: 'action', key: 'action', fixed: 'right', width: 200,
                 render: (rowValue, row, index) => {
@@ -100,10 +131,7 @@ export default class WxPayTemplate extends Component {
                         </div>
                     )
                 }
-
             },
-
-
         ]
 
 

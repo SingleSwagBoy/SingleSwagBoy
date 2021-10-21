a3c13cacbdd628f5e793009b6c1b9a9a0bba82c2
sunlin <1164700321@qq.com>
2021-10-21 11:18:33 +0800

    奥运会专题视频编辑

--- src/pages/ayh/specialList/index.jsx
+++ src/pages/ayh/specialList/index.jsx
@@ -24,7 +24,6 @@ export default class SportsProgram extends Component {
       searchWords:"",
       lists: [],
       currentItem:"",//编辑行的id
-      currentVideoItem:"",  // 编辑视频 id
       newData:{},
       layout: {
         labelCol: { span: 4 },
@@ -191,21 +190,15 @@ export default class SportsProgram extends Component {
       videoVisible:false,
       videoColumns:[
         {
-          title: "序号",
-          dataIndex: "videoSort",
-          key: "videoSort",
+          title: "名称",
+          dataIndex: "title",
+          key: "title",
         },
         {
           title: "id",
           dataIndex: "pid",
           key: "pid",
         },
-        {
-          title: "名称",
-          dataIndex: "title",
-          key: "title",
-        },
-        
         {
           title: "操作",
           key: "action",
@@ -413,7 +406,6 @@ export default class SportsProgram extends Component {
   }
   cvideos(id){
     cvideos({column_id:id}).then(res=>{
-      console.log("获取的短视频列表",res)
       if(res.data.errCode === 0){
         this.setState({
           linkVideoList:res.data.data

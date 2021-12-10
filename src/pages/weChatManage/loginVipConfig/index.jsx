11c649e3a44b28ab45bb660714d47a4d7ff268c2
尹正超 <>
2021-12-10 15:20:25 +0800

    企业微信

--- src/pages/weChatManage/loginVipConfig/index.jsx
+++ src/pages/weChatManage/loginVipConfig/index.jsx
@@ -43,7 +43,10 @@ export default class EarnIncentiveTask extends React.Component {
                 }
             },
             allUserList: [],
-            myUserList: [],
+            chooseUserList: [],
+            noSavebtn:[],
+            filterList:[],
+            userType:null,
             columns: [
                 {
                     title: "位置",
@@ -127,7 +130,7 @@ export default class EarnIncentiveTask extends React.Component {
         }
     }
     render() {
-        let { lists, layout, loading, columns, entranceState, tagList, allUserList, myUserList, userEdit } = this.state;
+        let { lists, layout, loading, columns, entranceState, tagList, allUserList,userType, chooseUserList,noSavebtn, userEdit,filterList } = this.state;
         return (
             <div className="loginVip">
                 <Card title={
@@ -162,7 +165,7 @@ export default class EarnIncentiveTask extends React.Component {
                             ref={this.formRef}
                             onFinish={this.submitForm.bind(this)}>
                             <Form.Item label="800客服联系人" name="tagCode">
-                                <div className="add_user" onClick={() => this.addUser()}><PlusOutlined />添加成员</div>
+                                <div className="add_user" onClick={() => this.addUser(1)}><PlusOutlined />添加成员</div>
                                 <div className="user_box">
                                     {
                                         this.getMyUserList(1).map(r => {
@@ -172,7 +175,7 @@ export default class EarnIncentiveTask extends React.Component {
                                 </div>
                             </Form.Item>
                             <Form.Item label="200客服联系人" name="tagCode">
-                                <div className="add_user" onClick={() => this.addUser()}><PlusOutlined />添加成员</div>
+                                <div className="add_user" onClick={() => this.addUser(2)}><PlusOutlined />添加成员</div>
                                 <div className="user_box">
                                     {
                                         this.getMyUserList(2).map(r => {
@@ -209,24 +212,60 @@ export default class EarnIncentiveTask extends React.Component {
                     }
                 </Modal>
                 <Modal title="客服编辑" centered visible={userEdit} onCancel={() => {
-                    this.setState({ userEdit: false })
+                    this.setState({ userEdit: false,noSavebtn:chooseUserList})
                 }} footer={null} width={800}>
                     <div className="add_dialog">
                         <div className="left">
-                            <Input.Search placeholder="请搜索你想要添加的客服" className="search_input" />
+                            <Input placeholder="请搜索你想要添加的客服" className="search_input" allowClear 
+                            onChange={(e)=>{ //过滤搜索
+                                let list = this.state.allUserList.filter(item=>item.name.includes(e.target.value))
+                                if(list.length>0){
+                                    this.setState({filterList:list})
+                                }else{
+                                    this.setState({filterList:[]})
+                                }
+                            }}
+                            />
+                            <div>全部成员</div>
                             <div className="list">
-                                <div className="every_list">
-                                    <img src="" alt="" />
-                                    <div>"name"</div>
-                                </div>
+                                {
+                                    filterList.map((r,i) => {
+                                        return (
+                                            <div className={`every_list ${this.getSplitArr(userType,r)?'activityClass':''}`}
+                                            key={i}
+                                            onClick={()=>{
+                                                let arr = noSavebtn.filter(item => item.type == userType)
+                                                if(arr.length>0){
+                                                    let list = arr[0].userids?arr[0].userids.split(","):[]
+                                                    let info = list.filter(l=>l == r.userid)
+                                                    if(info.length == 0){ //增加
+                                                        list.push(r.userid)
+                                                        arr[0].userids = list.join(",")
+                                                    }else{ //删除
+                                                        let h = list.filter(l=>l != r.userid)
+                                                        arr[0].userids = h.join(",")
+                                                    }
+                                                    console.log(111)
+                                                    this.setState({noSavebtn:arr})
+                                                }
+                                            }}
+                                            >
+                                                <img src={r.avatar} alt="" />
+                                                <div>{r.name}</div>
+                                            </div>
+                                        )
+                                    })
+                                }
+
                             </div>
                         </div>
                         <div className="left">
-                            <div className="list">
+                            <div className="list" key={noSavebtn.length}>
+                                <div>已选客服</div>
                                 {
-                                    this.getMyUserList(1).map(r => {
+                                    this.getMyUserList(userType,1).map((r,i) => {
                                         return (
-                                            <div className="every_list">
+                                            <div className="every_list" key={i}>
                                                 <img src={r.avatar} alt="" />
                                                 <div>{r.name}</div>
                                                 <div><CloseOutlined /></div>
@@ -238,17 +277,39 @@ export default class EarnIncentiveTask extends React.Component {
                             </div>
                         </div>
                     </div>
+                    <div className="user_btn"><Button>取消</Button><Button type="primary" onClick={()=>{
+                        console.log(this.getMyUserList(userType))
+                    }}>确定</Button></div>
                 </Modal>
             </div>
         )
     }
-    getMyUserList(type) {
-        let arr = this.state.myUserList.filter(item => item.type == type)
+    getSplitArr(type,r){ //匹配哪些已经被选择了
+        let arr = this.state.noSavebtn.filter(item => item.type == type)
+        if(arr.length>0){
+            let list = arr[0].userids?arr[0].userids.split(","):[]
+            if(list.length>0){
+                let info = list.filter(item=>item == r.userid)
+                if(info.length>0){
+                    return true
+                }else{
+                    return false
+                }
+            }else{
+                return false
+            }
+        }else{
+            return false
+        }
+        
+    }
+    getMyUserList(type,source) { //匹配初始选中的客服
+        let arr = (source== 1?this.state.noSavebtn:this.state.chooseUserList).filter(item => item.type == type)
         if (arr.length > 0) {
             let h = arr[0].userids ? arr[0].userids.split(",") : []
             let l = this.state.allUserList.filter(item => h.some(l => item.userid == l))
             if (l.length > 0) {
-                console.log(l)
+                // console.log(l)
                 return l
             } else {
                 return []
@@ -297,14 +358,20 @@ export default class EarnIncentiveTask extends React.Component {
     getWechatUser() {  //获取全部客服
         getWechatUser({}).then(res => {
             this.setState({
-                allUserList: res.data
+                allUserList: res.data,
+                filterList:res.data
             })
         })
     }
     getMyWechatUser() {  //获取全部客服
         getMyWechatUser({}).then(res => {
+            let info = res.data
             this.setState({
-                myUserList: res.data
+                chooseUserList: info,
+            },()=>{
+                this.setState({
+                    noSavebtn:JSON.parse(JSON.stringify(this.state.chooseUserList))
+                })
             })
         })
     }
@@ -339,7 +406,7 @@ export default class EarnIncentiveTask extends React.Component {
             return ""
         }
     }
-    addUser() {
-        this.setState({ userEdit: true })
+    addUser(type) {
+        this.setState({ userEdit: true,userType:type,noSavebtn:this.state.chooseUserList })
     }
 }

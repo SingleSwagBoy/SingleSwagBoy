af932b8a0a9defc068905b3d7df6693cc7d776dd
尹正超 <>
2021-12-23 14:47:44 +0800

    拼团机器人

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -68,6 +68,7 @@ const RecommendConfig = lazy(() => import('pages/configManage/recommendConfig/in
 const WordsSwiperConfig = lazy(() => import('pages/configManage/wordsSwiperConfig/index.jsx'));
 const HomeBroadcast = lazy(() => import('pages/configManage/homeBroadcast/index.jsx'));
 const EnterChannelConfig = lazy(() => import('pages/configManage/enterChannelConfig/index.jsx'));
+const PuzzleRobot = lazy(() => import('pages/configManage/puzzleRobot/index.jsx'));
 const ProgramAppConfig = lazy(() => import('pages/programApp/ProgramAppConfig/index.jsx')); //电视节目单配置
 const LuckyDraw=lazy(() => import('pages/activeManagement/luckyDraw/index.jsx'))
 const GoodsConfig=lazy(() => import('pages/activeManagement/goodsConfig/index.jsx'))
@@ -147,6 +148,7 @@ const adminRoutes = [
     { name: '文字轮播配置', icon: UnorderedListOutlined, path: '/mms/config/wordsSwiperConfig', component: WordsSwiperConfig, code: "configManage", sub_code: 'wordsSwiperConfig' },
     { name: '首页直播配置', icon: UnorderedListOutlined, path: '/mms/config/homeBroadcast', component: HomeBroadcast, code: "configManage", sub_code: 'homeBroadcast' },
     { name: '开机进入频道配置', icon: UnorderedListOutlined, path: '/mms/config/enterChannelConfig', component: EnterChannelConfig, code: "configManage", sub_code: 'enterChannelConfig' },
+    { name: '拼团机器人', icon: UnorderedListOutlined, path: '/mms/config/puzzleRobot', component: PuzzleRobot, code: "configManage", sub_code: 'puzzleRobot' },
 
 
 

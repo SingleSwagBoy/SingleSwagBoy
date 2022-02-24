e3cc6266b0ee82a41afff4bd569c9bd507abb8d6
尹正超 <>
2022-02-24 17:48:19 +0800

    风险设备控制加上同步缓存

--- src/pages/offlineManage/riskControl/index.jsx
+++ src/pages/offlineManage/riskControl/index.jsx
@@ -85,7 +85,7 @@ function App2() {
               setOpen(true)
               setSource("add")
             }}>新建</Button> */}
-            {/* <MySyncBtn type={7} name='同步缓存' /> */}
+            <MySyncBtn type={30} name='同步缓存' />
           </div>
         }
       >

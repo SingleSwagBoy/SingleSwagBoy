a0e694eb461452d7c30a826050bdd46df4c62cd5
尹正超 <>
2022-05-05 19:35:55 +0800

    优化组件里面的参数，增加参数解释

--- src/components/jumpType/channel.js
+++ src/components/jumpType/channel.js
@@ -20,8 +20,8 @@ let privateData = {
     inputTimeOutVal: null
 };
 function App2(props) {
-    const [forceUpdateId, forceUpdate] = useReducer(() => [], []);
-    const [forceUpdatePage, forceUpdatePages] = useReducer(() => [], []);
+    // const [forceUpdateId, forceUpdate] = useReducer(() => [], []);
+    // const [forceUpdatePage, forceUpdatePages] = useReducer(() => [], []);
     const { formRef } = props
     const [channelList, setChannelList] = useState([])
     const selectProps = {
@@ -33,9 +33,9 @@ function App2(props) {
             console.log('onSearch')
         }
     }
-    useEffect(()=>{
+    useEffect(() => {
         getChannelList()
-    },[])
+    }, [])
     //搜索频道
     const comChannel = (type, e) => {
         if (privateData.inputTimeOutVal) {
@@ -61,17 +61,24 @@ function App2(props) {
         })
     }
     return (
+        // 参数
+        // formName：选择框的名称，string
+        // channelCode：表单里面的字段 string
+        // isRequired：是否必填boolean
+        // multiple：是否多选boolean
+        // isLink ：是否有联动 boolean
+        // linkData：联动数组["form里面的字段","频道列表里面的字段"]
+        // onForceUpdatePages ：更新Function
         <div>
-            <Form.Item label={props.formName||"频道"} name={props.channelCode}  rules={[{ required:props.isRequired, message: `请选择${props.formName||"频道"}` }]}>
-                <Select style={{ width: "100%" }} placeholder={`请选择${props.formName||"频道"}`} allowClear mode={props.multiple}
+            <Form.Item label={props.formName || "频道"} name={props.channelCode} rules={[{ required: props.isRequired, message: `请选择${props.formName || "频道"}` }]}>
+                <Select style={{ width: "100%" }} placeholder={`请选择${props.formName || "频道"}`} allowClear mode={props.multiple}
                     {...selectProps}
                     onSearch={(e) => comChannel(1, e)}
-                    onChange={(e)=>{
-                        //是否有联动 联动的字段props.linkData["form里面的字段","频道列表里面的字段"]
-                        if(props.isLink && props.linkData){
-                            let arr = channelList.filter(item=>item.code == e)
-                            console.log({[props.linkData[0]]:arr[0][props.linkData]})
-                            formRef.setFieldsValue({[props.linkData[0]]:arr[0][props.linkData[1]]})
+                    onChange={(e) => {
+                        if (props.isLink && props.linkData) {
+                            let arr = channelList.filter(item => item.code == e)
+                            console.log({ [props.linkData[0]]: arr[0][props.linkData] })
+                            formRef.setFieldsValue({ [props.linkData[0]]: arr[0][props.linkData[1]] })
                             props.onForceUpdatePages()
                         }
                     }}

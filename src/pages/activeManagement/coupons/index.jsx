557f7cf9a04d4fe23edd57e0dd59c5b6611e7fc9
sunlin <1164700321@qq.com>
2022-05-18 15:30:49 +0800

    修改折扣的文案显示问题

--- src/pages/activeManagement/coupons/index.jsx
+++ src/pages/activeManagement/coupons/index.jsx
@@ -81,8 +81,8 @@ function App2() {
             <div>
                 {
                     rowValue==1 &&
-                    <span>折扣-{row.deductNum}折</span> ||
-                    <span>直减-减{row.deductNum}</span>
+                    <span>折扣-{row.deductNum/10}折</span> ||
+                    <span>直减-减{row.deductNum/100}元</span>
                 }
             </div>
           )
